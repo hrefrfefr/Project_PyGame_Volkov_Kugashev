8b58dfa08831e7828d799d3830ee4f515df79206
vkv1004 <75637047+vkv1004@users.noreply.github.com>
2021-03-07 17:25:12 +0400

    main.py
    1

--- main.py
+++ main.py
@@ -65,3 +65,72 @@ def write_Text(text, font, surface, x, y):
 def terminate():
     pygame.quit()
     sys.exit()
+    
+    
+def crash(playerRect, asteroids):
+    for a in asteroids[::-1]:
+        if playerRect.colliderect(a['rect']):
+            return True
+    return False
+
+
+def start_screen():
+    intro_text = ["Командир!", "SOS!!!",
+                  "Мы попали в пояс астероидов",
+                  "ускорение на Ctrl",
+                  "удачи, конец связи шшшш..."]
+    fon = pygame.transform.scale(load_image('background.png'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    pygame.mixer.music.load(os.path.join('data', 'startscreenmusic.mp3'))
+    pygame.mixer.music.play(-1, 0.0)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+player_image = load_image('Plane.png', -1)
+asteroid_image = load_image('asteroid.png', -1)
+high_speed_player_image = load_image('Plane speed.png', -1)
+f = load_image('f.png', -1)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(width // 2, height // 2)
+        self.speed = 3
+        self.rechargetime = 600
+        self.highspeed = False
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def speed_down(self):
+        self.cur_tick = pygame.time.get_ticks()
+        self.speed = 3
+        self.highspeed = False
+        self.image = player_image
+
+    def speed_up(self):
+        self.cur_tick = pygame.time.get_ticks()
+        self.speed = 10
+        self.highspeed = True
+        self.image = high_speed_player_image
+
+    def kill(self):
+        self.image = f
